a9524dc09fb714155e27f114a359e6fa33b1d830
samumirandam <samuelchampi@gmail.com>
2024-06-14 18:54:30 -0500

    feat: add intro section

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,6 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   singleQuote: false,
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#fcf6f5",
+        text: "#03090a",
+      },
+      backgroundImage: {
+        flowers: "url('/flowers-kotkoa.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
